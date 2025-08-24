c47dfe9f67ce280d8dfa73981c92cf3ab5e7a320
Aditya <adityasi127@gmail.com>
2025-08-25 04:37:04 +0530

    added yml file and app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
+if __name__ == "__main__":
+    print("5 + 3 =", add(5, 3))
